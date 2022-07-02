67ad3e267d7cef95518b61bbca51bf2a348f0642
harisov-mobile <v8spec@mail.ru>
2022-07-02 18:45:23 +0300

    Add out to sealed class Result.kt

--- domain/src/main/java/ru/internetcloud/bookcatalog/domain/model/Result.kt
+++ domain/src/main/java/ru/internetcloud/bookcatalog/domain/model/Result.kt
@@ -1,8 +1,8 @@
 package ru.internetcloud.bookcatalog.domain.model
 
-sealed class Result<T> {
+sealed class Result<out T> {
 
-    data class Success<T>(val data: T) : Result<T>()
+    data class Success<out T>(val data: T) : Result<T>()
 
     data class Error(val exception: Exception) : Result<Nothing>()
 
